f08c33f781a730da9148f4b870e719dccced73fc
Joschi96 <117238956+Joschi96@users.noreply.github.com>
2024-08-25 10:51:46 +0200

    added first logic behind displaying numbers

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 // Initializing variables for operations.
 // A calculator operation will consist of a 
 // number, an operator, and another number. For example, 3 + 5.
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 let op = '';
 let runningTotal = 0;
 let displayValue = '0';
 
-let btnNum = container.querySelector("button")
+const btnNum = document.querySelector('button');
+const screen = document.querySelector('.screen');
+
+calcButtons = document.querySelector('.calc-buttons').addEventListener('click', (event)=>buttonClick(event.target.innerText));
 
 function buttonClick(value) {
     if (isNaN(value)) {
         handleSymbol(value);
     } else{
         handleNumber(value);
+    }
+    screen.innerText = displayValue;
+}
+
+function handleNumber(num){
+    if (op === '') {
+        num1 += num;
+        displayValue = num1;
+    } else {
+        num2 += num;
+        displayValue = num2;
     }
 }
 
@@ -22,13 +36,34 @@ function handleSymbol(symbol) {
         case 'C':
             displayValue = '0';
             runningTotal = 0;
+            num1=num2=op='';
             break;
+
         case '←':
             if (displayValue.length === 1) {
                 displayValue = '0';
             } else{
                 displayValue = displayValue.substring(0,displayValue.length-1);
             }
+            break;
+        
+        case '=':
+            runningTotal=operate(op,num1,num2);
+            displayValue = runningTotal.toString();
+
+        case '−':
+            op = '−';
+            displayValue = '0';
+            break;
+        
+        case '+':
+            break;
+        
+        case '×':
+            break;
+
+        case '÷':
+            break;
     
         default:
             break;
@@ -64,19 +99,19 @@ function divide(a,b) {
 // and then calls one of the basic math operator functions on the numbers.
 function operate(operator, a, b) {
     switch (operator) {
-        case '&plus':
+        case '+':
             add(a,b);
             break;
         
-        case '&minus':
+        case '−':
             subtract(a,b);
             break;
 
-        case '&times':
+        case '×':
             multiply(a,b);
             break;
         
-        case '&divide':
+        case '÷':
             divide(a,b);
             break;
 
@@ -84,3 +119,5 @@ function operate(operator, a, b) {
             break;
     }
 }
+
+console.log(operate('+',23,21))
